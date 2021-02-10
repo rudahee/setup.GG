b5f60ace6cc2e89fd517a4b3311b02f802d6b7d2
rudahee <rudahee@gmail.com>
2021-02-10 20:00:02 +0100

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD2GIgQRsSoRjjIptcno6o0PBgR_lgpbQA",
+    authDomain: "db-setupgg.firebaseapp.com",
+    projectId: "db-setupgg",
+    storageBucket: "db-setupgg.appspot.com",
+    messagingSenderId: "485855756221",
+    appId: "1:485855756221:web:03614f95b90574dce71cd4"
+  }
 };
